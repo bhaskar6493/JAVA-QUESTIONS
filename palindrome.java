f37cd3abba984eb1540c2775e342716207b20def
Bhaskar Das <bhaskar6493@gmail.com>
2026-09-13 12:30:05 +0530

    palindrome number

---
+++ palindrome.java
@@ -0,0 +1,19 @@
+public class palindrome {
+    public static void main(String[] args) {
+        int number = 121;
+        int originalNumber = number;
+        int reverse = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number = number / 10;
+        }
+
+        if (originalNumber == reverse) {
+            System.out.println(originalNumber + " is a palindrome number");
+        } else {
+            System.out.println(originalNumber + " is not a palindrome number");
+        }
+    }
+}
