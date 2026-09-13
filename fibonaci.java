afe1e4ecc59249b72e54a8eeeaf02979b6a40814
Bhaskar Das <bhaskar6493@gmail.com>
2026-09-13 12:08:41 +0530

    fibonacci series

---
+++ fibonaci.java
@@ -0,0 +1,16 @@
+public class fibonaci {
+    public static void main(String[] args) {
+        int first = 0;
+        int second = 1;
+
+        System.out.println("Fibonacci series:");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
